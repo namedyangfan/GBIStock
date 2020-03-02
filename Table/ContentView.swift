7704cb8438189b890498c2110a03169e3233b6b1
fan <cedric.yf@gmail.com>
2020-03-02 16:30:07 -0500

    Initial Commit

---
+++ Table/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Table
+//
+//  Created by Fan Yang on 2020-03-02.
+//  Copyright © 2020 Fan Yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
